f5eebdaf3ca584d7b3788fee802fd258e2126449
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2024-11-16 17:56:56 +0900

    feat: css리셋 적용
    emotion normalize를 이용해 css 리셋 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import { css, Global } from "@emotion/react";
+import Home from "./pages/Home";
+import emotionNormalize from "emotion-normalize";
+
 export function App() {
-  return <></>;
+  return (
+    <>
+      <Global
+        styles={css`
+          ${emotionNormalize}
+          html,
+    body {
+            padding: 0;
+            margin: 0;
+            background: white;
+            min-height: 100%;
+            font-family: Helvetica, Arial, sans-serif;
+          }
+        `}
+      />
+      <Home />
+    </>
+  );
 }
 
 export default App;

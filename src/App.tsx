b6e14ef61f4eed28f4baae07cad7d472dac7a439
stillcorners <yheedev@gmail.com>
2024-11-16 17:56:30 +0900

    feat: normalize 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { theme } from '../Theme';
+import { ThemeProvider } from '@mui/material/styles';
+import { Global as EmotionGlobal } from '@emotion/react';
+import emotionNormalize from 'emotion-normalize';
+
 export function App() {
-  return <></>;
+  return (
+    <ThemeProvider theme={theme}>
+      <EmotionGlobal styles={emotionNormalize} />
+    </ThemeProvider>
+  );
 }
 
-export default App;
+// export default App;
